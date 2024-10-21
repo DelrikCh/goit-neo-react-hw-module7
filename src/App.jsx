8470a12cfb0d9488cc1578955cd4d1b88eb48974
Roman Choporov <ChoporovRoman@gmail.com>
2024-10-21 01:42:07 -0700

    Complete task

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
-import './App.css'
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchContacts } from "./redux/contactsOps";
+import ContactForm from "./components/ContactForm/ContactForm";
+import ContactList from "./components/ContactList/ContactList";
+import SearchBox from "./components/SearchBox/SearchBox";
 
-function App() {
+const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <>
-      <h1>Hello World!</h1>
-    </>
-  )
-}
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
+  );
+};
 
-export default App
+export default App;
